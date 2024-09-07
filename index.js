e8e6dfca956726afdf5a7ac743c12ac77b90d3c9
TioTeuss <113296442+TioTeuss@users.noreply.github.com>
2024-09-07 13:18:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Bot feito por: tioteus.","#PJLTEUS"];
 
 
 let currentIndex = 0;
